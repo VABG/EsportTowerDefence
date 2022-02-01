68314c85e7bef462e10faae4547f50b100b30a0e
VABG <VABGProductions@hotmail.com>
2022-02-01 09:41:50 +0100

    Project Commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] float moveSpeedMult = 20;
+    Vector3 moveTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveTarget = transform.position;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            move += Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            move += Vector3.back;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            move += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            move += Vector3.right;
+        }
+
+        move.Normalize();
+        moveTarget += move * moveSpeedMult * Time.deltaTime;
+
+        transform.position = Vector3.Lerp(transform.position, moveTarget, Time.deltaTime * 5.0f);
+
+    }
+}
